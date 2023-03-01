25b5dd921e8debb9d82dd7d3ea2468d33d2d8641
shinypve <tri598110@gmail.com>
2023-03-01 09:34:52 +0700

    Main Chuyển sang tiếng Việt và thêm nút login

--- login.php
+++ login.php
@@ -1,30 +1,36 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng nhập</title>
 </head>
 
 <body>
-    <h2>Login Form</h2>
-    <form method="post" action="login.php">
-        <label>Tên đăng nhập:</label>
-        <input type="text" name="username" required><br><br>
-        <label>Mật khẩu:</label>
-        <input type="password" name="password" required><br><br>
-        <input type="submit" name="login" value="Login">
-    </form>
-    <?php
-    if (isset($_POST['login'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        if ($username == "user" && $password == "password") {
-            echo "Login successful";
-        } else {
-            echo "Incorrect password";
+
+    <body>
+        <h2>Login Form</h2>
+        <form method="post" action="login.php">
+            <label>Username:</label>
+            <input type="text" name="username" required><br><br>
+            <label>Password:</label>
+            <input type="password" name="password" required><br><br>
+            <input type="submit" name="login" value="Login">
+        </form>
+        <?php
+        if (isset($_POST['login'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            if ($username == "user" && $password == "password") {
+                echo "Đăng nhập thành công";
+            } else {
+                echo "Sai mật khẩu";
+            }
         }
-    }
-    ?>
+        ?>
+    </body>
 </body>
 
 </html>
