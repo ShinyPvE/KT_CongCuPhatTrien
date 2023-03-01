6b1a14a04db2d8544253ec11020ed29ad589109a
shinypve <tri598110@gmail.com>
2023-03-01 09:11:45 +0700

    Main Thay đổi label thành tên đăng nhập và mật khẩu

--- login.php
+++ login.php
@@ -1,27 +1,30 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Login Form</title>
+    <title>Đăng nhập</title>
 </head>
+
 <body>
-	<h2>Login Form</h2>
-	<form method="post" action="login.php">
-		<label>Username:</label>
-		<input type="text" name="username" required><br><br>
-		<label>Password:</label>
-		<input type="password" name="password" required><br><br>
-		<input type="submit" name="login" value="Login">
-	</form>
-	<?php
-		if(isset($_POST['login'])){
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-			if($username == "user" && $password == "password"){
-				echo "Login successful";
-			}else{
-				echo "Invalid login credentials";
-			}
-		}
-	?>
+    <h2>Login Form</h2>
+    <form method="post" action="login.php">
+        <label>Tên đăng nhập:</label>
+        <input type="text" name="username" required><br><br>
+        <label>Mật khẩu:</label>
+        <input type="password" name="password" required><br><br>
+        <input type="submit" name="login" value="Login">
+    </form>
+    <?php
+    if (isset($_POST['login'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        if ($username == "user" && $password == "password") {
+            echo "Login successful";
+        } else {
+            echo "Incorrect password";
+        }
+    }
+    ?>
 </body>
+
 </html>
